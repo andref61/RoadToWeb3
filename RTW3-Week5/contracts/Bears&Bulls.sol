0e5e8a04831aa1431b3f2155ac3a0f4c6e460d5c
Andref61 <aferrara61@gmail.com>
2022-06-18 13:29:49 -0400

    Price Feed

--- RTW3-Week5/contracts/Bears&Bulls.sol
+++ RTW3-Week5/contracts/Bears&Bulls.sol
@@ -20,8 +20,13 @@ contract BearsBulls is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable, Keep
 
     uint    lastTimeStamp;
     uint     interval;
+    int256 public currentPrice;
 
+    AggregatorV3Interface public pricefeed;
     Counters.Counter private _tokenIdCounter;
+
+    event TokensUpdated(string marketTrend);
+    event TokenMinted(uint256 tokenId);
 
     // IPFS URIs for the dynamic nft graphics/metadata.
     string[] bullUrisIpfs = [
@@ -38,15 +43,99 @@ contract BearsBulls is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable, Keep
     constructor( uint _interval) ERC721("Bulls&Bears", "BBTK") {
         interval = _interval;
         lastTimeStamp = block.timestamp;
+
+        /**
+        * Network: Rinkeby
+        * Data Feed: ETH/USD
+        * Address: 0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419
+        */
+        pricefeed = AggregatorV3Interface(0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419);
     }
 
-    function safeMint(address to, uint256 tokenId, string memory uri)
-        public
-        onlyOwner
-    {
-        _safeMint(to, tokenId);
-        _setTokenURI(tokenId, uri);
+    function safeMint(address to) public {
+        // minted token id is the counter current value
+        uint256 tokenId = _tokenIdCounter.current();
+
+        // increment token Id counter for next mint
         _tokenIdCounter.increment();
+
+        // mint it
+        _safeMint(to, tokenId);
+
+        // Default to a bull NFT
+        string memory defaultUri = bullUrisIpfs[0];
+        _setTokenURI(tokenId, defaultUri);
+
+        emit TokenMinted((tokenId));
+     }
+
+    function checkUpkeep(bytes calldata /* checkData */) external view override returns (bool upkeepNeeded, bytes memory /*performData */) {
+        upkeepNeeded = (block.timestamp - lastTimeStamp) > interval;
+        // We don't use the checkData in this example. The checkData is defined when the Upkeep was registered.
+    }
+
+    function performUpkeep(bytes calldata /* performData */) external override {
+        //We highly recommend revalidating the upkeep in the performUpkeep function
+        if ((block.timestamp - lastTimeStamp) > interval ) {
+            lastTimeStamp = block.timestamp;
+            int latestPrice =  getLatestPrice(); 
+        
+            if (latestPrice == currentPrice) {
+                return;
+            }
+
+            if (latestPrice < currentPrice) {
+                // bear
+                updateAllTokenUris("bear");
+
+            } else {
+                // bull
+                updateAllTokenUris("bull");
+            }
+
+            // update currentPrice
+            currentPrice = latestPrice;
+        } else {
+            return;
+        }
+    }
+
+    function getLatestPrice() public view returns (int256) {
+         (
+            /*uint80 roundID*/,
+            int price,
+            /*uint startedAt*/,
+            /*uint timeStamp*/,
+            /*uint80 answeredInRound*/
+        ) = pricefeed.latestRoundData();
+
+        return price; //  example price returned 3034715771688
+    }
+
+    function updateAllTokenUris(string memory trend) internal {
+        if (compareStrings("bear", trend)) {
+             for (uint i = 0; i < _tokenIdCounter.current() ; i++) {
+                _setTokenURI(i, bearUrisIpfs[0]);
+            } 
+            
+        } else {     
+             for (uint i = 0; i < _tokenIdCounter.current() ; i++) {
+                _setTokenURI(i, bullUrisIpfs[0]);
+            }  
+        }   
+        emit TokensUpdated(trend);
+    }
+
+    function setPriceFeed(address newFeed) public onlyOwner {
+        pricefeed = AggregatorV3Interface(newFeed);
+    }
+    function setInterval(uint256 newInterval) public onlyOwner {
+        interval = newInterval;
+    }
+    
+
+    function compareStrings(string memory a, string memory b) internal pure returns (bool) {
+        return (keccak256(abi.encodePacked((a))) == keccak256(abi.encodePacked((b))));
     }
 
     // The following functions are overrides required by Solidity.
@@ -78,19 +167,5 @@ contract BearsBulls is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable, Keep
         returns (bool)
     {
         return super.supportsInterface(interfaceId);
-    }
-
-    function checkUpkeep(bytes calldata /* checkData */) external view override returns (bool upkeepNeeded, bytes memory /*performData */) {
-        upkeepNeeded = (block.timestamp - lastTimeStamp) > interval;
-        // We don't use the checkData in this example. The checkData is defined when the Upkeep was registered.
-    }
-
-    function performUpkeep(bytes calldata /* performData */) external override {
-        //We highly recommend revalidating the upkeep in the performUpkeep function
-        if ((block.timestamp - lastTimeStamp) > interval ) {
-            lastTimeStamp = block.timestamp;
- //           counter = counter + 1;
-        }
-        // We don't use the performData in this example. The performData is generated by the Keeper's call to your checkUpkeep function
     }
 }
